09fefe65bc156eb4349e99c49574f3f5abafe9b4
Alex Kravchuk <akravchuk89@gmail.com>
2015-01-20 11:51:22 +0200

    Clean up views

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,6 +1,6 @@
 class Operator < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable, :registerable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
   has_many :orders
 end
